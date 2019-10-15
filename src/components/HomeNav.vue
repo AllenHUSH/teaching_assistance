130e7a865a9907c7f61e740ac3a6344a92c9f6a7
Allen <shunhanghu@foxmail.com>
2019-10-15 18:31:57 +0800

    fix

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -2,7 +2,10 @@
   <van-collapse v-model="activeNames">
     <van-collapse-item title="标题1" name="1">内容</van-collapse-item>
     <van-collapse-item title="标题2" name="2">内容</van-collapse-item>
-    <van-collapse-item title="标题3" name="3" disabled>内容</van-collapse-item>
+    <van-collapse-item title="系统设置" name="3" icon="setting-o">
+      <van-cell value="语言" is-link />
+      <van-cell value="密码" is-link />
+    </van-collapse-item>
   </van-collapse>
 </template>
 <script>
